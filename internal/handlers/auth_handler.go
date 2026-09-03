e35073d0383ca14245aac707bc8b785f50620740
itadory 2007 <nanamiitadory495@gmail.com>
2026-09-03 20:07:24 +0330

    added Signup function

--- internal/handlers/auth_handler.go
+++ internal/handlers/auth_handler.go
@@ -18,3 +18,25 @@ func NewAuthHandler(userService *service.UserService, sessionService *service.Se
 		sessionService: sessionService,
 	}
 }
+
+func (h *AuthHandler) Signup(c echo.Context) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	user, err := h.userService.Register(c.Request().Context(), username, password,)
+	if err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+	token, err := h.sessionService.CreateSession(c.Request().Context(), user.ID,)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "failed to create session",)
+	}
+	cookie := new(http.Cookie)
+	cookie.Name = "session_token"
+	cookie.Value = token
+	cookie.Path = "/"
+	cookie.HttpOnly = true
+	cookie.MaxAge = 24 * 60 * 60
+	c.SetCookie(cookie)
+	return c.Redirect(http.StatusSeeOther, "/")
+}
