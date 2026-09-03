dce23da192422ae7628aa99423a625cbf777f983
itadory 2007 <nanamiitadory495@gmail.com>
2026-09-03 20:08:47 +0330

    added Login function

--- internal/handlers/auth_handler.go
+++ internal/handlers/auth_handler.go
@@ -22,10 +22,30 @@ func NewAuthHandler(userService *service.UserService, sessionService *service.Se
 func (h *AuthHandler) Signup(c echo.Context) error {
 	username := c.FormValue("username")
 	password := c.FormValue("password")
-
 	user, err := h.userService.Register(c.Request().Context(), username, password,)
 	if err != nil {
 		return c.String(http.StatusBadRequest, err.Error())
+	}
+	token, err := h.sessionService.CreateSession(c.Request().Context(), user.ID,)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "failed to create session",)
+	}
+	cookie := new(http.Cookie)
+	cookie.Name = "session_token"
+	cookie.Value = token
+	cookie.Path = "/"
+	cookie.HttpOnly = true
+	cookie.MaxAge = 24 * 60 * 60
+	c.SetCookie(cookie)
+	return c.Redirect(http.StatusSeeOther, "/")
+}
+
+func (h *AuthHandler) Login(c echo.Context) error {
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+	user, err := h.userService.Login(c.Request().Context(), username, password,)
+	if err != nil {
+		return c.String(http.StatusUnauthorized, err.Error())
 	}
 	token, err := h.sessionService.CreateSession(c.Request().Context(), user.ID,)
 	if err != nil {
